cf6eabd6bc5590a418780ee7ce43b4948b2d6caf
arieltecay <arieltecay@gmail.com>
2022-05-19 21:34:48 -0300

    Update clases

--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,7 @@
 /* Practicar en agregar el reject en cada promesa y permutar los errores */
 /* Verificar tambien los errores en el promise.all */
 
-function descargarClientes() {
+/* function descargarClientes() {
     console.log('Descargando Clientes...');
     return new Promise(resolve => {
         setTimeout(() => {
@@ -17,15 +17,69 @@ function descargarProductos() {
             resolve('Productos descargado')
         }, 1000)
     })
+} */
+
+function descargarClientes() {
+    console.log('Descargando Clientes...');
+    return new Promise((resolve, reject) => {
+        const valor = true;
+        setTimeout(() => {
+            (valor) ?
+                (resolve('Cliente descargado.'))
+                :
+                (reject("El cliente no se ha descargado."));
+        }, 5000)
+    })
+}
+
+function descargarProductos() {
+    console.log('Descargando Productos...');
+    return new Promise((resolve, reject) => {
+        const valor = true;
+        setTimeout(() => {
+            (valor) ? resolve('Producto descargado.') : reject("El producto no se ha descargado.");
+        }, 1000)
+    })
 }
 
 const app = async () => {
     try {
         /*         
+        const clientes = await descargarClientes();
+        console.log(clientes);
+        
+        const productos = await descargarProductos();
+        console.log(productos); */
+
+        const respuesta = await Promise.allSettled([descargarClientes(), descargarProductos()])
+        mostrarHTML(respuesta);
+        console.log("seguimos");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function mostrarHTML(respuesta) {
+    const body = document.querySelector('body')
+    respuesta.map(dato => {
+        console.log(dato.value);
+        const title = document.createElement('h3')
+        title.textContent = dato.value
+        body.appendChild(title)
+    })
+}
+
+app()
+
+
+/* const app = async () => {
+    try {
+                
         const clientes = await descargarClientes()
         console.log(clientes);
         const productos = await descargarProductos()
-        console.log(productos); */
+        console.log(productos);
 
         const respuesta = await Promise.all([descargarClientes(), descargarProductos()])
         console.log(respuesta[1]);
@@ -36,3 +90,4 @@ const app = async () => {
 }
 
 app()
+ */
--- js/app3.js
+++ js/app3.js
@@ -4,15 +4,61 @@ const body = document.querySelector("body")
 document.addEventListener('DOMContentLoaded', function () {
     obtenerDatos()
 })
+/*Segundo metodo aprendido Convertir a Async Await*/
+
 
 /* Primer metodo aprendido */
-const obtenerDatos = () => {
+/* const obtenerDatos = () => {
     fetch(url)
         .then(datos => datos.json())
-        .then(photos => mostrar(photos))
+        .then(photos => console.log(photos))
         .catch(error => console.log(error))
+} */
+
+/* async function obtenerDatos() { */
+
+const obtenerDatos = async () => {
+    try {
+        const resultado = await fetch(url);
+        const resulJson = await resultado.json();
+        mostrarHTML(resulJson);
+
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+const mostrarHTML = (datos) => {
+    datos.map(dato => {
+        const { id, author, post_url } = dato
+        const div = document.createElement('div')
+        div.innerHTML =
+            "<div class='nombre-clase' id='id' >ID:" + id + "</div>" +
+            "<div>Nombre del Autor:" + author + "</div>" +
+            "<a target='_blank' href='" + post_url + "'>Ver imagen</a>"
+        body.appendChild(div)
+    })
 }
 
 
-/*Segundo metodo aprendido Convertir a Async Await*/
+/* function mostrarHTML(datos) {
+    console.log(datos);
 
+    datos.map(dato => {
+        const { author, id, post_url } = dato;
+        const html = document.createElement('div')
+        html.innerHTML = `        
+            <p> ----------------</p>
+            <p class="parrafo"> Autor: ${author}</p>
+            <p> Id: ${id} </p>
+            <a href="${post_url}" target="_blank">ver imagen</a>
+            <p> ----------------</p>        
+        `
+        body.appendChild(html)
+    });
+    const hoja = document.querySelector("body > div:nth-child(7)")
+    hoja.classList.add('parrafo2')
+} */
